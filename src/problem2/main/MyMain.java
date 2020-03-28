6f1f55f6428dd8025aa3ee1f3c70391ed757cd92
shubham2249 <62793760+shubham2249@users.noreply.github.com>
2020-03-28 21:08:51 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,22 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyQueue object = new MyQueue();
+        System.out.println("enter the number of elements you want to enter in the tree");
+        int sizeOfTree = sc.nextInt();
+        for (int i = 0; i < sizeOfTree; i++) {
+            System.out.println("enter the value to insert in the tree");
+            int value = sc.nextInt();
+            object.insert(value);
+        }
+        int middle = sizeOfTree / 2;
+        object.postorder(object.root);
+        object.preorder(object.root);
+        object.printPreOrder();
+        object.printPostOrder();
+        object.checkOpposites();
+        object.checkMiddles(middle);
+    }
 }
