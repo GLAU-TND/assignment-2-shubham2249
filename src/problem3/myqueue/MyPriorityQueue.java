afe27a9bade149bb88f21e80e236133f77ecd0b3
shubham2249 <62793760+shubham2249@users.noreply.github.com>
2020-03-28 21:11:10 +0530

    Update MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -7,4 +7,33 @@
 package problem3.myqueue;
 
 public class MyPriorityQueue {
+   Node front = null;
+    Node rear = null;
+
+    public void insertAndArrange(int valueToBeInserted) {
+        Node node = new Node(valueToBeInserted);
+        if (front == null && rear == null) {
+            front = rear = node;
+        } else if (node.getRollNumber() < front.getRollNumber()) {
+            node.setNext(front);
+            front = node;
+        } else if (node.getRollNumber() > rear.getRollNumber()) {
+            rear.setNext(node);
+            rear = node;
+        } else {
+            Node current = front;
+            while (current.getNext().getRollNumber() < node.getRollNumber()) {
+                current = current.getNext();
+            }
+            node.setNext(current.getNext());
+            current.setNext(node);
+        }
+    }
+
+    public void display() {
+        Node current = front;
+        System.out.println("the list of arranged rollnumbers is:");
+        for (current = front; current != null; current = current.getNext())
+            System.out.print(current.getRollNumber() + "----->");
+    }
 }
