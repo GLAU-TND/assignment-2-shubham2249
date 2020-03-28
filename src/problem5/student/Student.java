e1034b20c5f21c7a06e857e4d0ba7cd05d8ca6d5
shubham2249 <62793760+shubham2249@users.noreply.github.com>
2020-03-28 21:24:24 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,15 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+  public String name;
+    public int rollNumber;
+    public int numberOfBacklogs;
+    public int appearingCount;
+
+    public Student(String name, int rollNumber, int numberOfBacklogs, int appearingCount) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.numberOfBacklogs = numberOfBacklogs;
+        this.appearingCount = appearingCount;
+    }
 }
