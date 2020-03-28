8337abd48aec9ed7ddba0971c53053821a35eefd
shubham2249 <62793760+shubham2249@users.noreply.github.com>
2020-03-28 21:12:26 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,27 @@
 package problem3.node;
 
 public class Node {
+  private int rollNumber;
+    private Node next;
+
+    public Node(int rollNumber) {
+        this.rollNumber = rollNumber;
+        this.next = null;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
